ca2ea672acbc4af629f9c07a026d487c8cd68b9c
Islam Sayed <islam.sayed8@gmail.com>
2025-04-08 18:27:55 +0200

    Add addAppToUser reducer to manage saved apps in user state

--- src/redux/slices/user.ts
+++ src/redux/slices/user.ts
@@ -1,18 +1,23 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AppType } from '../../types';
 
 interface User {
   jwtToken: string;
   refreshToken: string;
 }
 
+
+
 interface UserState {
   user: User | null;
   isAuthenticated: boolean;
+  savedApps: AppType[] | null;
 }
 
 const initialState: UserState = {
   user: null,
   isAuthenticated: false,
+  savedApps: null,
 };
 
 const userSlice = createSlice({
@@ -27,8 +32,19 @@ const userSlice = createSlice({
       state.user = null;
       state.isAuthenticated = false;
     },
+    addAppToUser: (state, action: PayloadAction<{ title: string; description: string; imageUrl: string; categoryValue: string; category: string; path: string }>) => {
+      if (state.savedApps) {
+        if (!state.savedApps.find((app) => app.title === action.payload.title)) {
+          state.savedApps.push(action.payload);
+        } else {
+          state.savedApps = state.savedApps.filter((app) => app.title !== action.payload.title);
+        }
+      } else {
+        state.savedApps = [action.payload];
+      }
+    }
   },
 });
 
-export const { setUser, removeUser } = userSlice.actions;
+export const { setUser, removeUser, addAppToUser } = userSlice.actions;
 export default userSlice.reducer;
