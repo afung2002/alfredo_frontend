bce298b7eb1e3c3e8024ee7712307e19c59a4bcb
Islam Sayed <islam.sayed8@gmail.com>
2025-04-08 18:25:06 +0200

    Refactor main.tsx to integrate Redux and PersistGate for state management

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react';
+import { persistor, store } from './redux/store.ts';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const clerkPubKey = import.meta.env.VITE_CLERK_KEY;
 createRoot(document.getElementById('root')!).render(
+
     <ClerkProvider publishableKey={clerkPubKey}>
-        <App />
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
     </ClerkProvider>
 )
