d3fea4020bfdcca3dffc8d811b79f304ea99c173
Islam Sayed <islam.sayed8@gmail.com>
2025-04-08 18:27:44 +0200

    Implement Redux store with persistence for user state management

---
+++ src/redux/store.ts
@@ -0,0 +1,33 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import { baseApi } from '@services/api/baseApi';
+import userReducer from './slices/user';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user',],
+};
+
+const rootReducer = combineReducers(
+  {
+    user: userReducer,
+    // [baseApi.reducerPath]: baseApi.reducer,
+  },
+);
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }).concat(baseApi.middleware),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
