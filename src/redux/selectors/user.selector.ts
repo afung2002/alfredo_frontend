22cce473964b08b3175902d8164dec8661e129df
Islam Sayed <islam.sayed8@gmail.com>
2025-04-09 22:06:14 +0200

    Fix formatting in user.selector.ts by ensuring consistent newline at the end of the file.

--- src/redux/selectors/user.selector.ts
+++ src/redux/selectors/user.selector.ts
@@ -1,3 +1,5 @@
 import { RootState } from '../store';
 
 export const selectUserApps = (state: RootState) => state.user.savedApps;
+
+export const selectUserInvestments = (state: RootState) => state.user.investments;
