881976e0fd5b593fe1aa169127c8fa21274d5aac
Islam Sayed <islam.sayed8@gmail.com>
2025-04-09 22:05:51 +0200

    Refactor AppsPage component to integrate Input for search functionality, enhancing user experience and code clarity.

--- src/pages/AppsPage/index.tsx
+++ src/pages/AppsPage/index.tsx
@@ -6,109 +6,103 @@ import {
   TextField,
 } from '@mui/material';
 import AppCard from '@components/AppCard';
-import {APP_CARDS} from '@constants/appCards';
+import { APP_CARDS } from '@constants/appCards';
 import { APPS_FILTER_TABS } from '@constants/index';
 import { useDispatch } from 'react-redux';
-import { addAppToUser } from '../../redux/slices/user';
+import { addAppToUser } from '@redux/slices/user';
 import { AppType } from '../../types';
-import { searchByTitle } from '../../utils/uiUtils';
+import { searchByTitle } from '@utils/uiUtils';
+import Input from '../../components/Input';
+import { useForm } from 'react-hook-form';
 
 const AppsPage = () => {
+  const { control, watch, setValue } = useForm({
+    defaultValues: {
+      'searchApps': ''
+    }
+  });
+  const searchValue = watch('searchApps');
   const dispatch = useDispatch();
   const [filteredCards, setFilteredCards] = useState(APP_CARDS);
   const [selectedTab, setSelectedTab] = useState('all');
-  const [searchTerm, setSearchTerm] = useState('');
+
   useEffect(() => {
     const filteredCards = APP_CARDS.filter((card) => {
       if (selectedTab === 'all') return true;
       return card.categoryValue === selectedTab;
     });
     setFilteredCards(filteredCards);
-    setSearchTerm('');
+    setValue('searchApps', '');
   }, [selectedTab]);
 
   const handleToggleApp = (card: AppType) => {
     dispatch(addAppToUser(card))
   };
+
+  useEffect(() => {
+    const filteredCards = searchByTitle(APP_CARDS || [], searchValue, 'title');
+    setFilteredCards(filteredCards);
+  }, [searchValue]);
   return (
     <div className='min-h-screen w-full p-4 flex-1'>
-        <Typography variant="h5" sx={{ mb: 2, fontWeight: 500, textAlign: 'left'}}>
-          Cutting-Edge Applications for Startup Investing
-        </Typography>
-          <TextField
-            value={searchTerm}
-            onChange={(e) => {
-              setSearchTerm(e.target.value);
-              const filteredCards = searchByTitle(APP_CARDS, e.target.value, 'title');
-              console.log('Filtered Cards:', filteredCards);
-              setFilteredCards(filteredCards);
-            }}
-            fullWidth
-            placeholder="Search applications..."
-            variant="outlined"
-            sx={{
-              mb: 2,
-              "& fieldset": {
-                borderColor: "rgba(0, 0, 0, 0.10)",
-              },
-              "& .MuiOutlinedInput-root": {
-                borderRadius: "28px",
-                height: "36px",
-              },
-            }}
-          />
+      <Typography variant="h5" sx={{ mb: 2, fontWeight: 500, textAlign: 'left' }}>
+        Cutting-Edge Applications for Startup Investing
+      </Typography>
+      <Input
+        type="text"
+        name="searchApps"
+        control={control}
+        placeholder="Search Apps..."
+        className="flex flex-col mb-4"
+      />
+      <Tabs
+        className='mb-2'
+        value={selectedTab}
+        onChange={(_, newValue) => setSelectedTab(newValue)}
+        variant="scrollable"
+        scrollButtons={false}
+        TabIndicatorProps={{ style: { display: 'none' } }}
 
-          <Tabs
-            value={selectedTab}
-            onChange={(_, newValue) => setSelectedTab(newValue)}
-            variant="scrollable"
-            scrollButtons={false}
-            TabIndicatorProps={{ style: { display: 'none' } }}
-            sx={{
-              mb: 1,
-              minHeight: '36px',
-              '& .MuiTabs-flexContainer': {
-                gap: 1,
-              },
-              '& .MuiTab-root': {
-                minHeight: '32px',
-                padding: '6px 16px',
-                borderRadius: '16px',
-                fontSize: '0.875rem',
-                textTransform: 'none',
-                border: '1px solid transparent',
-                '&.Mui-selected': {
-                  border: '1px solid',
-                  borderColor: 'rgba(0, 0, 0, 0.2)',
-                  backgroundColor: 'transparent',
-                  color: 'black',
-                },
-              },
-            }}
-          >
-            {APPS_FILTER_TABS.map((tab) => (
-              <Tab
-                key={tab.value}
-                label={tab.label}
-                value={tab.value}
-                disableRipple
-              />
-            ))}
-          </Tabs>
-      
-        <div className="flex flex-col gap-2 min--[600px]">
-          {filteredCards.map((card, index) => (
-            <AppCard
-              key={index}
-              title={card.title}
-              description={card.description}
-              imageUrl={card.imageUrl}
-              category={card.category}
-              path={card.path}
-              onAdd={() => handleToggleApp(card)}
-            />
-          ))}
-        </div>
+      >
+        {APPS_FILTER_TABS.map((tab) => (
+          <Tab
+          sx={{
+            minHeight: 32,
+            minWidth: 'auto',
+            px: 4,
+            borderRadius: '50px',
+            textTransform: 'none',
+            bgcolor: tab.value === selectedTab ? 'primary.main' : 'grey.200',
+            color: tab.value === selectedTab ? 'white' : 'black',
+            mx: 1,
+            fontSize: 14,
+            fontWeight: 500,
+            '&.Mui-selected': {
+              bgcolor: 'primary.main',
+              color: 'white',
+            },
+          }}
+            key={tab.value}
+            label={tab.label}
+            value={tab.value}
+            disableRipple
+          />
+        ))}
+      </Tabs>
+
+      <div className="flex flex-col gap-4 min-w-[600px]">
+        {filteredCards.map((card, index) => (
+          <AppCard
+            key={index}
+            title={card.title}
+            description={card.description}
+            imageUrl={card.imageUrl}
+            category={card.category}
+            path={card.path}
+            onAdd={() => handleToggleApp(card)}
+          />
+        ))}
+      </div>
     </div>
   )
 }
