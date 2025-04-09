193b8a4ca35c483d6bf743fd30172727eedc3321
Islam Sayed <islam.sayed8@gmail.com>
2025-04-09 17:48:22 +0200

    Add ThemeProvider to main.tsx for theme management

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,19 @@ import { ClerkProvider } from '@clerk/clerk-react';
 import { persistor, store } from './redux/store.ts';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
+import { ThemeProvider } from '@mui/material/styles';
+import { darkTheme, lightTheme } from './theme';
+const mode = 'light'
 const clerkPubKey = import.meta.env.VITE_CLERK_KEY;
 createRoot(document.getElementById('root')!).render(
 
     <ClerkProvider publishableKey={clerkPubKey}>
         <Provider store={store}>
+        <ThemeProvider theme={mode === 'dark' ? darkTheme : lightTheme}>
             <PersistGate loading={null} persistor={persistor}>
                 <App />
             </PersistGate>
+            </ThemeProvider>
         </Provider>
     </ClerkProvider>
 )
