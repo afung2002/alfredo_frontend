6988c4c7ad96ae0af07e11d8d4cfe6d04f17ccbd
Islam Sayed <islam.sayed8@gmail.com>
2025-04-10 17:36:30 +0200

    Refactor InvestmentsList component to simplify props and streamline investment rendering logic

--- src/components/InvestmentsList/index.tsx
+++ src/components/InvestmentsList/index.tsx
@@ -1,71 +1,34 @@
+
+
 import { Typography } from "@mui/material"
 import InvestmentCard from "../InvestmentCard"
 import { useNavigate } from 'react-router';
 import { noDataMessageStyles } from "@src/utils/uiUtils";
 import { Routes } from "@src/constants/routes";
+import { InvestmentDetails } from "@src/types";
 
 type InvestmentsListProps = {
-  selectedTab: string;
-  filteredInvestments?: InvestmentDetails[] | undefined;
-  filteredFundInvestments?: InvestmentDetails[];
-  filteredAngelInvestments?: InvestmentDetails[];
+  investments: InvestmentDetails[] | undefined;
 }
-import { InvestmentDetails } from "@src/types";
-const InvestmentsList = ({selectedTab, filteredInvestments, filteredFundInvestments, filteredAngelInvestments }: InvestmentsListProps) => {
+const InvestmentsList = ({investments }: InvestmentsListProps) => {
   const navigate = useNavigate();
   return (
     <>
-    {
-      filteredInvestments && filteredInvestments.length && 
-      filteredInvestments?.map((investment, index) => (
-        <InvestmentCard
-          key={index}
-          investment={investment}
-          onClick={() => navigate(Routes.FUND_MANAGER_INVESTMENT.replace(':investmentId', investment.id))}
-        />
-      ))}
-    
-    {selectedTab === "all" && filteredInvestments?.map((investment, index) => (
-      <InvestmentCard
-        key={index}
-        investment={investment}
-        onClick={() => navigate(Routes.FUND_MANAGER_INVESTMENT.replace(':investmentId', investment.id))}
-      />
-    ))}
-
-    {selectedTab === "fund" && (
-      filteredFundInvestments &&
-      filteredFundInvestments.length > 0 ? (
-        filteredFundInvestments.map((investment, index) => (
-          <InvestmentCard
-            key={index}
-            investment={investment}
-
-          />
-        ))
-      ) : (
-        <Typography variant="body1" sx={noDataMessageStyles}>
-          No fund investments found.
-        </Typography>
-      )
-    )}
-
-    {selectedTab === "angel" && (
-      filteredAngelInvestments && 
-      filteredAngelInvestments.length > 0 ? (
-        filteredAngelInvestments.map((investment, index) => (
-          <InvestmentCard
-            key={index}
-            investment={investment}
-            onClick={() => navigate(Routes.FUND_MANAGER_INVESTMENT.replace(':investmentId', investment.id))}
-          />
-        ))
-      ) : (
-        <Typography variant="body1" sx={noDataMessageStyles}>
-          No angel investments found.
-        </Typography>
-      )
-    )}
+      {
+        investments && investments.length > 0 ? (
+          investments.map((investment, index) => (
+            <InvestmentCard
+              key={index}
+              investment={investment}
+              onClick={() => navigate(Routes.FUND_MANAGER_INVESTMENT.replace(':investmentId', investment.id))}
+            />
+          ))
+        ) : (
+          <Typography variant="body1" sx={noDataMessageStyles}>
+            No investments found.
+          </Typography>
+        )
+      }
     </>
     
   )
