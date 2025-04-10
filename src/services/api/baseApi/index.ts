63185c86c59da98c423df28d6bfa96b15fbbe365
Islam Sayed <islam.sayed8@gmail.com>
2025-04-10 17:37:18 +0200

    Refactor API endpoints to use constants for URLs and improve query structure

--- src/services/api/baseApi/index.ts
+++ src/services/api/baseApi/index.ts
@@ -1,29 +1,50 @@
 // src/features/api/apiSlice.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import { Tags } from '@constants/tags';
+import { Endpoints } from '@constants/endpoints';
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl:import.meta.env.VITE_BASE_URL }),
   endpoints: (builder) => ({
     // Companies endpoint
-    getCompanies: builder.query({
-      query: () => '/companies',
+    getCompanies: builder.query<Company[], void>({
+      query: () => ({
+        url: Endpoints.COMPANIES,
+        method: 'GET',
+      }),
+      providesTags: [Tags.COMPANIES],
     }),
     // Investments endpoint
     getInvestments: builder.query({
-      query: () => '/investments',
+      query: () => ({
+        url: Endpoints.INVESTMENTS,
+        method: 'GET',
+      }),
+      providesTags: [Tags.INVESTMENTS],
     }),
     // Funds endpoint
     getFunds: builder.query({
-      query: () => '/funds',
+      query: () => ({
+        url: Endpoints.FUNDS,
+        method: 'GET',
+      }),
+      providesTags: [Tags.FUNDS],
     }),
     // Documents endpoint
     getDocuments: builder.query({
-      query: () => '/documents',
+      query: () => ({
+        url: Endpoints.DOCUMENTS,
+        method: 'GET',
+      }),
+      providesTags: [Tags.Documents],
     }),
     // Partners endpoint
     getPartners: builder.query({
-      query: () => '/partners',
+      query: () => ({
+        url: Endpoints.LIMITED_PARTNERS,
+        method: 'GET',
+      }),
+      providesTags: [Tags.LIMITED_PARTNERS],
     }),
   }),
 });
