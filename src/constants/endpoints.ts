d410322c4376075ee04b22acd9b54901685761da
Islam Sayed <islam.sayed8@gmail.com>
2025-04-11 19:04:09 +0200

    Update endpoints to include trailing slashes for consistency

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,9 +1,10 @@
 export enum Endpoints {
-  COMPANIES = 'companies',
-  INVESTMENTS = 'investments',
-  FUNDS = 'funds',
-  DOCUMENTS = 'documents',
-  PARTNERS = 'partners',
-  USERS = 'users',
-  LIMITED_PARTNERS = 'limited-partners',
+  COMPANIES = 'companies/',
+  INVESTMENTS = 'investments/',
+  FUNDS = 'funds/',
+  DOCUMENTS = 'documents/',
+  PARTNERS = 'partners/',
+  USERS = 'users/',
+  LIMITED_PARTNERS = 'limited-partners/',
+  FUND_UPDATES = 'fund-updates/',
 }
