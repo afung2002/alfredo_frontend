89021582ddb02054d3c08a18836519bca5729dae
Islam Sayed <islam.sayed8@gmail.com>
2025-04-11 19:03:33 +0200

    Add FundUpdateModal component for creating fund updates with form validation

---
+++ src/components/FundUpdateModal/index.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Modal, Box, Typography, Dialog } from "@mui/material";
+import Button from "@components/Button";
+import Input from "@components/Input";
+
+type FundUpdateModalProps = {
+  open: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  control: any;
+  errors: {
+    postTitle?: { message?: string };
+    postDescription?: { message?: string };
+  };
+};
+
+const FundUpdateModal: React.FC<FundUpdateModalProps> = ({
+  open,
+  onClose,
+  onSubmit,
+  control,
+  errors,
+}) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      aria-labelledby="create-fund-update-modal"
+    >
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          boxShadow: 24,
+          p: 4,
+          borderRadius: 1,
+          width: '600px'
+        }}
+      >
+        <Typography variant="h6" component="h2" sx={{ mb: 3, color: "black" }}>
+          Create Fund Update
+        </Typography>
+
+        <form onSubmit={onSubmit}>
+          <div className="mb-4">
+            <Input
+              name="postTitle"
+              control={control}
+              label="Title"
+              rounded={false}
+              error={!!errors?.postTitle?.message}
+            />
+          </div>
+
+          <Input
+            name="postDescription"
+            control={control}
+            label="Message"
+            multiline
+            rows={5}
+            rounded={false}
+            error={!!errors?.postDescription?.message}
+          />
+
+          <Box
+            className="mt-4 gap-2"
+            sx={{ display: "flex", justifyContent: "flex-end" }}
+          >
+            <Button variant="outlined" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button type="submit">Post</Button>
+          </Box>
+        </form>
+      </Box>
+    </Dialog>
+  );
+};
+
+export default FundUpdateModal;
