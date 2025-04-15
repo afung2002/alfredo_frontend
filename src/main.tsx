180d7f85545f96e395409782b4e046e2a16bb0ae
Islam Sayed <islam.sayed8@gmail.com>
2025-04-15 21:31:09 +0200

    Refactor theme provider to use a single theme import

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,18 @@ import { persistor, store } from './redux/store.ts';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from '@mui/material/styles';
-import { darkTheme, lightTheme } from './theme';
-const mode = 'light'
+import theme from './theme/index.ts';
+
 const clerkPubKey = import.meta.env.VITE_CLERK_KEY;
 createRoot(document.getElementById('root')!).render(
 
     <ClerkProvider publishableKey={clerkPubKey}>
         <Provider store={store}>
-        {/* <ThemeProvider theme={mode === 'dark' ? darkTheme : lightTheme}> */}
+        <ThemeProvider theme={theme}>
             <PersistGate loading={null} persistor={persistor}>
                 <App />
             </PersistGate>
-            {/* </ThemeProvider> */}
+            </ThemeProvider>
         </Provider>
     </ClerkProvider>
 )
