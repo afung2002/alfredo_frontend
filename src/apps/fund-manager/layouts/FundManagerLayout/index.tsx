3383d2f8c5c6e5dc87c989adc5eedc49bdc0e6d7
Islam Sayed <islam.sayed8@gmail.com>
2025-04-15 21:31:34 +0200

    Enhance FundManagerLayout by adding icons to side menu buttons and adjusting padding

--- src/apps/fund-manager/layouts/FundManagerLayout/index.tsx
+++ src/apps/fund-manager/layouts/FundManagerLayout/index.tsx
@@ -2,21 +2,25 @@ import { Box } from "@mui/material";
 import { Outlet } from "react-router";
 import AppSideMenu from "@components/AppSideMenu";
 import { Routes } from "@constants/routes";
+import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const FundManagerLayout = () => {
   const sideMenuButtons = [
-    { label: 'All', value: 'all', path: Routes.FUND_MANAGER },
-    { label: 'Funds', value: 'funds', path: Routes.FUND_MANAGER_FUNDS },
-    { label: 'Documents', value: 'documents', path: Routes.FUND_MANAGER_DOCUMENTS },
+    { label: 'Investments', value: 'all', path: Routes.FUND_MANAGER, icon: <AccountBalanceIcon /> },
+    { label: 'Funds', value: 'funds', path: Routes.FUND_MANAGER_FUNDS, icon: <TrendingUpIcon /> },
+    { label: 'Documents', value: 'documents', path: Routes.FUND_MANAGER_DOCUMENTS, icon: <FolderOpenOutlinedIcon /> },
     // { label: 'Limited Partners', value: 'limited-partners', path: '/fundmanager-ai/limited-partners' },
     // { label: 'Settings', value: 'settings', path: '/fundmanager-ai/settings' },
-    { label: 'Back to Apps', value: 'back-to-apps', path: Routes.APPS },
+    { label: 'Back to Apps', value: 'back-to-apps', path: Routes.APPS, icon: <ArrowBackIcon /> },
   ];
   return (
-    <Box sx={{ display: 'flex', minHeight: '100vh' }} className="bg-gray-50">
+    <Box sx={{ display: 'flex', minHeight: '100vh' }}>
       <AppSideMenu sideMenuButtons={sideMenuButtons} />
       <div 
-        className="flex-1 xl:px-60 md:px-32 sm:px-16 px-4 pt-24 pb-24"
+        className="flex-1 xl:px-64 lg:px-44 md:px-28 sm:px-16 px-4 pt-24 pb-24"
       >
         <Outlet />
       </div>
