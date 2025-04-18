bac23b6f0d7a9dcefcf3048e7fb6718dafe3ed9d
Islam Sayed <islam.sayed8@gmail.com>
2025-04-18 18:47:00 +0200

    feat: Remove ThemeProvider from main application entry

--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,17 @@ import { ClerkProvider } from '@clerk/clerk-react';
 import { persistor, store } from './redux/store.ts';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme/index.ts';
+
 
 const clerkPubKey = import.meta.env.VITE_CLERK_KEY;
+
 createRoot(document.getElementById('root')!).render(
 
     <ClerkProvider publishableKey={clerkPubKey}>
         <Provider store={store}>
-        <ThemeProvider theme={theme}>
             <PersistGate loading={null} persistor={persistor}>
                 <App />
             </PersistGate>
-            </ThemeProvider>
         </Provider>
     </ClerkProvider>
 )
