1fd5d880e013d89f237266ea064b6f28e8eaf5c7
islamCodehood <islam.sayed8@gmail.com>
2025-04-21 18:21:09 +0200

    feat: Implement LimitedPartnerDocuments page with document search and upload functionality

---
+++ src/apps/limited-partner/pages/LimitedPartnerDocuments/index.tsx
@@ -0,0 +1,116 @@
+import { useEffect, useState } from 'react';
+import { Box, Typography, CircularProgress, Alert, Chip, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { Document } from '../../../../types';
+import UploadDocumentModal from '@components/UploadDocumentModal';
+import { searchByTitle } from '@utils/uiUtils';
+import Input from '@components/Input';
+import { useForm } from 'react-hook-form';
+import Button from '@components/Button';
+import DocumentsList from '@components/DocumentsList';
+import { useGetDocumentsQuery } from '@services/api/baseApi';
+import { Apps } from '@src/constants/apps';
+
+const LimitedPartnerDocuments = () => {
+  const { data: documentsData, isLoading: isLoadingDocuments, error: errorDocuments } = useGetDocumentsQuery();
+
+  const { control, watch } = useForm({
+    defaultValues: {
+      'searchDocuments': ''
+    }
+  });
+  const searchValue = watch('searchDocuments');
+  const [isUploadModalOpen, setIsUploadModalOpen] = useState(false);
+  const [filteredDocs, setFilteredDocs] = useState<Document[]>(documentsData || []);
+  const [selectedOrientation, setSelectedOrientation] = useState<"row" | "grid">('row');
+  const handleOrientationChange = (event: React.MouseEvent<HTMLElement>, newOrientation: "row" | "grid") => {
+    if (newOrientation !== null) {
+      setSelectedOrientation(newOrientation);
+    }
+  };
+  useEffect(() => {
+    if (searchValue === '') {
+      setFilteredDocs(documentsData || []);
+      return;
+    }
+    const filteredDocs = searchByTitle(documentsData, searchValue, 'name') || [];
+    setFilteredDocs(filteredDocs);
+  }, [searchValue, documentsData]);
+
+
+  // useEffect(() => {
+  //   const filteredDocs = documents.filter((doc) => {
+  //     if (selectedTab === 'all') return true;
+  //     return doc.type === selectedTab;
+  //   });
+  //   setFilteredDocs(filteredDocs);
+  //   setValue('searchDocuments', '');
+  // }, [selectedTab, documents, setValue]);
+  const handleModalClose = () => {
+    setIsUploadModalOpen(false);
+  }
+
+  if (isLoadingDocuments) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="60vh">
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (errorDocuments) {
+    return (
+      <Box p={3}>
+        <Alert severity="error">
+          <>{errorDocuments}</></Alert>
+      </Box>
+    );
+  }
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+          {/* Document Statistics */}
+          <Box className="flex gap-3 items-center" sx={{ mb: 1 }}>
+            <Typography variant="h3" sx={{ mb: 1, fontWeight: 600, textAlign: 'left' }}>
+              Documents
+            </Typography>
+            <Chip
+              label={documentsData?.length}
+              color="secondary"
+              sx={{ fontSize: '0.875rem', fontWeight: 700, borderRadius: '4px' }}
+            />
+          </Box>
+    
+          <Box sx={{ display: "flex", gap: 2, mb: 2, width: '100%' }}>
+    
+            <Input
+              type="text"
+              name="searchDocuments"
+              control={control}
+              placeholder="Search documents..."
+              className="flex flex-col w-full"
+            />
+    
+          </Box>
+          {/* <div className="flex justify-end mb-2">
+            <ToggleButtonGroup  color="secondary" value={selectedOrientation} exclusive onChange={handleOrientationChange}>
+              <ToggleButton  value="grid" onClick={() => setSelectedOrientation('grid')} sx={{ textTransform: 'none' }}>
+                <AppsIcon sx={{ fontSize: '1.2rem' }} />
+              </ToggleButton>
+              <ToggleButton value="row" onClick={() => setSelectedOrientation('row')} sx={{ textTransform: 'none' }}>
+                <TableRowsIcon sx={{ fontSize: '1.2rem' }} />
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </div> */}
+    
+          <DocumentsList app={Apps.LIMITED_PARTNER} selectedOrientation={selectedOrientation}  documents={filteredDocs} isLoading={isLoadingDocuments} />
+    
+          {/* Upload Modal */}
+          <UploadDocumentModal
+            open={isUploadModalOpen}
+            onClose={handleModalClose}
+          />
+        </Box>
+  )
+}
+
+export default LimitedPartnerDocuments
