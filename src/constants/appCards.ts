47fbe6e4097366d4dcc81cc5fec5e00d915890df
Islam Sayed <islam.sayed8@gmail.com>
2025-04-24 15:09:50 +0200

    style: Update image imports and improve titles in APP_CARDS constant

--- src/constants/appCards.ts
+++ src/constants/appCards.ts
@@ -1,14 +1,16 @@
-import AppImage1 from '@assets/appImage1.svg';
-import AppImage2 from '@assets/appImage2.svg';
-import AppImage3 from '@assets/appImage3.svg';
-import AppImage4 from '@assets/appImage4.svg';
-import AppImage5 from '@assets/appImage5.svg';
-import AppImage6 from '@assets/appImage6.svg';
+import AppImage1 from '@assets/image-1.png';
+import AppImage2 from '@assets/image-2.png';
+import AppImage3 from '@assets/image-3.png';
+import AppImage4 from '@assets/image-4.png';
+import AppImage5 from '@assets/image-5.png';
+import AppImage6 from '@assets/image-6.png';
+import AppImage7 from '@assets/image-7.png';
+import AppImage8 from '@assets/image-8.png';
 import { Routes } from '@constants/routes';
 
 export const APP_CARDS = [
   {
-    title: 'Fundmanager.ai',
+    title: 'Fund Manager',
     description: 'AI-powered fund admin for nimble funds & super angels',
     imageUrl: AppImage1,
     category: 'For Investors',
@@ -16,7 +18,7 @@ export const APP_CARDS = [
     path: Routes.FUND_MANAGER,
   },
   {
-    title: 'Limitedpartner.ai',
+    title: 'Limited Partner',
     description: 'AI-powered control panel for high-performing limited partners ',
     imageUrl: AppImage2,
     category: 'For Limited Partners',
@@ -24,7 +26,7 @@ export const APP_CARDS = [
     path: Routes.LIMITED_PARTNER_FUNDS,
   },
   {
-    title: 'Findintros.ai',
+    title: 'Find Intros',
     description: 'Harness your investor ecosystem to pinpoint customers & request intros',
     imageUrl: AppImage3,
     category: 'For Founders',
@@ -32,7 +34,7 @@ export const APP_CARDS = [
     path: '/apps',
   },
   {
-    title: 'Companytracker.ai',
+    title: 'Company Tracker',
     description: 'Input investment prospects and track all relevant updates, in real-time',
     imageUrl: AppImage4,
     category: 'For Investors',
@@ -40,7 +42,7 @@ export const APP_CARDS = [
     path: '/apps',
   },
   {
-    title: 'VCassociate.ai',
+    title: 'VC Associate',
     description: 'Enter characteristics and comb target websites for relevant prospects',
     imageUrl: AppImage5,
     category: 'For Investors',
@@ -48,9 +50,25 @@ export const APP_CARDS = [
     path: '/apps',
   },
   {
-    title: 'Memogenerator.ai',
+    title: 'Memo Generator',
     description: 'AI-powered investment memo generation',
     imageUrl: AppImage6,
+    category: 'For Investors',
+    categoryValue: 'investors',
+    path: '/apps',
+  },
+  {
+    title: 'Automate Social Media Content Creation',
+    description: 'AI-powered Linkedin and Twitter post creation',
+    imageUrl: AppImage7,
+    category: 'For Investors',
+    categoryValue: 'investors',
+    path: '/apps',
+  },
+  {
+    title: 'AI Powered Investment Scoring',
+    description: 'Harness our proprietary investment rubric to score investment prospect characteristics',
+    imageUrl: AppImage8,
     category: 'For Investors',
     categoryValue: 'investors',
     path: '/apps',
