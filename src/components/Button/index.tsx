b1d0918d2422069917faf5a2eaa8a6d64187d5b5
Islam Sayed <islam.sayed8@gmail.com>
2025-04-25 17:37:32 +0300

    style: Update Button component to accept variant prop and clean up styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,22 +2,32 @@ import { Button as MuiButton, ButtonProps } from '@mui/material';
 
 interface IButtonProps extends ButtonProps {
   className?: string;
+  variant?: 'text' | 'outlined' | 'contained';
 }
 
-const Button = ({ className, children, ...props }: IButtonProps) => {
+const Button = ({ className, children, variant, ...props }: IButtonProps) => {
   return (
     <MuiButton
-      variant="contained"
-      size="small"
+      variant={variant ?? 'contained'}
+          sx={{
+            flexShrink: 0,
+            textTransform: "none",
+            bgcolor: "black",
+            color: "white",
+            borderRadius: "2px",
+            "&:hover": {
+              bgcolor: "rgba(0, 0, 0, 0.8)",
+            },
+          }}
       {...props}
       className={`shrink-0 px-4 ${className ?? ''}`}
-      sx={{
-        borderRadius: '50px',   // Fully rounded, like your OutlinedInput
-        boxShadow: 'none',      // Flat/2D style (no elevation)
-        textTransform: 'none',  // Keep original casing
-        fontWeight: 500,        // Consistent with Material Design text button
-        padding: '6px 16px',  // Consistent with Material Design text button
-      }}
+      // sx={{
+      //   borderRadius: '50px',   // Fully rounded, like your OutlinedInput
+      //   boxShadow: 'none',      // Flat/2D style (no elevation)
+      //   textTransform: 'none',  // Keep original casing
+      //   fontWeight: 500,        // Consistent with Material Design text button
+      //   padding: '6px 16px',  // Consistent with Material Design text button
+      // }}
     >
       {children}
     </MuiButton>
