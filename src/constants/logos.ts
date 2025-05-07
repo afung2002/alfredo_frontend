1128f841b7c5ef68af00a71f40927858ca8479a4
Islam Sayed <islam.sayed8@gmail.com>
2025-05-07 15:53:04 +0300

    feat: Introduce logos for Limited Partner and Fund Manager to enhance branding

---
+++ src/constants/logos.ts
@@ -0,0 +1,7 @@
+import LimitedPartner from '@assets/limited_partner.png';
+import FundManager from '@assets/fund_manager.png';
+
+export const Logos =  {
+  'LimitedPartner.ai': LimitedPartner,
+  'FundManager.ai': FundManager,
+}
