96bb59e9abb7249216bd52cd7a25f3205dcf21ca
Islam Sayed <islam.sayed8@gmail.com>
2025-05-22 18:51:49 +0300

    fix: comment out flexbox styles in Loader component for centering

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -2,7 +2,8 @@ import { CircularProgress } from "@mui/material";
 
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    // <div className="flex items-center justify-center h-screen">
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <CircularProgress
         size={50}
       />
